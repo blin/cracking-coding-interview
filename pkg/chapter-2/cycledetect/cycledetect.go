5fb82905f642276dde42b2a03a2b2cd9a49e0eab
Evgenii Petrov <blinunleius@gmail.com>
2021-06-05 10:38:07 +0100

    Update comment for DetectCycle

--- pkg/chapter-2/cycledetect/cycledetect.go
+++ pkg/chapter-2/cycledetect/cycledetect.go
@@ -16,10 +16,23 @@ func DetectCycle(l *slist.Element) (*slist.Element, bool) {
 	}
 	p2 := l.Next.Next
 
-	// if the loop is of size n
-	// p1 has a sequence [(i, i%n) for i in Z]
-	// p2 has a sequence [(i, (k+(i*2))%n) for i in Z]
-	// smallest i for which p1 == p2 seems to be n-k
+	// If the loop is of size n,
+	// after i steps
+	// p1 will be at position i%n
+	// p2 will be at position d+(i*2))%n
+	// where d is the distance between p1 and p2 when both enter the loop.
+	//
+	// Smallest i for which p1 == p2 is such that i == (n * (l1+l2)) - d
+	// where l1 and l2 are number of loops completed by p1 and p2 respectively.
+	//
+	// Since p1 advances slower than p2, l1 = 0 and l2 = 1 is the smallest combination of loops
+	// for which i == (n * (l1+l2)) - d holds true,
+	// and so smallest i == n - d .
+	//
+	// Given the above, time to find the loop is bound by O(n).
+	//
+	// Thanks to Daria Zarayskaya
+	// for helping me figure out the modular arithmetic for this algorithm.
 	detectionIndex := 0
 	for {
 		if p1 == p2 {
